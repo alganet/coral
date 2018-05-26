d512c3059a698a2aff4e9906aeb34024d445d811
Alexandre Gaigalas <alexandre.gaigalas@easytaxi.com.br>
2018-05-25 21:47:48 -0300

    proto

--- lib/fs/lines.sh
+++ lib/fs/lines.sh
@@ -5,25 +5,29 @@
 fs_lines ()
 {
 	local line
-	local fs_lines_ifs
+	local oldifs
+
+	# Removes the internal field separator
+	oldifs="${IFS}"
+	IFS=
 
 	if test -e "${1:-}"
 	then
 		# Reading from file descriptor
-		exec < "${1}"
+		while read -r line
+		do
+			printf '%s\n' "${line}"
+		done < "${1}"
+	else
+		# Reading from stdin
+		while read -r line
+		do
+			printf '%s\n' "${line}"
+		done
 	fi
 
-	# Removes the internal field separator
-	fs_lines_ifs="${IFS}"
-	IFS=
-
-	while read -r line
-	do
-		printf '%s\n' "${line}"
-	done
-
 	# Restores the internal field separator
-	IFS="${fs_lines_ifs}"
+	IFS="${oldifs}"
 
 	return 0
 }
