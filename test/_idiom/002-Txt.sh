057abf82dadae1fcdd7b3648c6542a7a1be751bf
Alexandre Gaigalas <alganet+github@gmail.com>
2024-07-06 17:26:34 -0300

    Introduce dump, rename Str to Txt, fixes namespace collision
    coral's pseudodatastructures use global counters to keep track of
    stored values. These are namespaced into four variables, which
    were _S _L _S _A. Reusing _S for sets and strings is no longer
    supported, and now strings have their own namespace _T for Txt.
    
    I also considered renaming `val` to `var`, then `Str` to `Val`, but
    that collides with some unstandard osh builtins, so we have to find
    another way. Txt sounds like a good compromise.
    
    This commit also introduces the dump function, which recursively
    builds a serialization of any pseudodatastructure reference.

--- test/_idiom/002-Txt.sh
+++ test/_idiom/002-Txt.sh
@@ -1,20 +1,20 @@
 # Copyright (c) Alexandre Gomes Gaigalas <alganet@gmail.com>
 # SPDX-License-Identifier: ISC
 
-test_Str_noargs () {
-    Str
+test_Txt_noargs () {
+    Txt
     eval deref=\$$_R
     tap_assert "$deref" ""
 }
 
-test_Str_unary_zerolength () {
-    Str ""
+test_Txt_unary_zerolength () {
+    Txt ""
     eval deref=\$$_R
     tap_assert "$deref" ""
 }
 
-test_Str_simple () {
-	Str "Some text"
+test_Txt_simple () {
+	Txt "Some text"
     eval deref=\$$_R
     tap_assert "$deref" "Some text"
 }
